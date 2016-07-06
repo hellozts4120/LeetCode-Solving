e90dd182107bf9cae6212ed8293fcdf8edb6e1bf
hellozts4120 <14307110190@fudan.edu.cn>
2016-07-06 20:35:53 +0800

    add Pascals Triangle

---
+++ Algorithms/pascals-triangle.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    var ans = [];
+
+    for (var i = 0; i < numRows; i++) {
+    if (i === 0) {
+        ans[i] = [1];
+        continue;
+    }
+    
+    ans[i] = [];
+    for (var j = 0; j <= i; j++)
+        if (j === 0) {
+            ans[i][j] = ans[i - 1][j];
+        } else if (j === i) {
+            ans[i][j] = ans[i - 1][j - 1];
+        } else { 
+            ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
+        }
+    }
+    
+    return ans;
+};
