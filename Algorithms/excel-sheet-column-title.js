a805f9992c7eab7fd67bd36d1ff48c90944f4e9b
hellozts4120 <14307110190@fudan.edu.cn>
2016-07-11 20:28:08 +0800

    add Excel Sheet Column Title

---
+++ Algorithms/excel-sheet-column-title.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var convertToTitle = function(n) {
+    var ans = '';
+    while (n) {
+        if (n % 26 === 0) {
+            ans += String.fromCharCode(26 + 64);
+            n = (n / 26) - 1;
+        }
+        else {
+            ans += String.fromCharCode(n % 26 + 64);
+            n = ~~(n / 26);
+        }
+    }
+
+    return ans.split('').reverse().join('');
+};
