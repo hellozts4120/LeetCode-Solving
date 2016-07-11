5c814b4110ca582fb4fc93922bdac12358f68cf9
hellozts4120 <14307110190@fudan.edu.cn>
2016-07-11 20:27:24 +0800

    add Excel Sheet Column Number

---
+++ Algorithms/excel-sheet-column-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var titleToNumber = function(s) {
+    var a = s.split('').reverse();
+    s = a.join('');
+    var ans = a.reduce(function(pre, item, index) {
+        return pre + Math.pow(26, index) * (s.charCodeAt(index) - 64);
+    }, 0);
+    return ans;
+};
