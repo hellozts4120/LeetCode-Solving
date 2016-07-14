1550d5aab5ead26c2047adf5057e24f6a6148bec
hellozts4120 <14307110190@fudan.edu.cn>
2016-07-14 20:09:40 +0800

    add Guess Number Higher Or Lower

---
+++ Algorithms/guess-number-higher-or-lower.cpp
@@ -0,0 +1,19 @@
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low = 1, high = n, mid, res;
+        while (low <= high) {
+            mid = low + (high - low) / 2;  
+            res = guess(mid);  
+            if(res == 1) low = mid + 1;  
+            else if(res == -1) high = mid - 1;  
+            else return mid;  
+        }
+        return low;
+    }
+};
