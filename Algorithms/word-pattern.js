795bccf2fd2e249368034dc386c46993ec95a772
hellozts4120 <14307110190@fudan.edu.cn>
2016-07-18 20:26:48 +0800

    add Word Pattern

---
+++ Algorithms/word-pattern.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} pattern
+ * @param {string} str
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, str) {
+    let arr = str.split(' ');
+
+    if (pattern.length !== arr.length)
+        return false;
+
+    let a2b = {}, b2a = {}, len = arr.length;
+    for (let i = 0; i < len; i++) {
+        let a = pattern[i], b = str[i];
+        
+        if (!a2b[a]) {
+            a2b[a] = b;
+        } else {
+            if (a2b[a] !== b) {
+                return false;
+            }
+        }
+        
+        if (!b2a[b]) {
+            b2a[b] = a;
+        } else {
+            if (b2a[b] !== a) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
